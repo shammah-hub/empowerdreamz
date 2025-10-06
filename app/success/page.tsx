cbe69981345dada45c365a19aa8056e76ae70220
Shammah <shammahmaida@yahoo.com>
2025-10-06 07:15:36 +0100

    increased donation amount value update 2

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
